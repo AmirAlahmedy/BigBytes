db7543e4fd4dd4ada44f33707d5ff7db4b3fee11
Amir Alahmedy <ameer.alahmedy@gmail.com>
2024-08-09 14:35:16 +0300

    Basic lexical analysis.

--- src/main/java/org/example/Lox.java
+++ src/main/java/org/example/Lox.java
@@ -6,10 +6,8 @@ import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Scanner;
+import java.util.List;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Lox {
     static boolean hadError = false;
 
